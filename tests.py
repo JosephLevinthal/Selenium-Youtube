fd26adf77dc2f0da291c2f5ce1d516ec4c4841ed
Joseph Levinthal <jvldo.hef@uea.edu.br>
2020-06-04 23:01:10 -0400

    Add tests.py
    Access home page of youtube

---
+++ tests.py
@@ -0,0 +1,17 @@
+#Import section
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Declare path for webdriver
+PATH = "chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.maximize_window()
+
+# Access Home Page
+website = 'https://youtube.com.br'
+driver.get(website)
+
